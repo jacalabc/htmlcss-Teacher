bb031c90af6d9be774e64b4445e184470e2bea9f
mackliu <mackliu@wda.gov.tw>
2023-01-04 11:11:10 +0800

    obj

--- s20230104/getData01.php
+++ s20230104/getData01.php
@@ -5,7 +5,11 @@
         echo '</pre>';
     }
 
-    $data = $_GET;
-    dd($data);
+    // $data = $_GET;
+    $data = $_POST;
+    // dd($data);
+    // echo "123";
+    // $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+    echo json_encode($data);
     
 ?>
