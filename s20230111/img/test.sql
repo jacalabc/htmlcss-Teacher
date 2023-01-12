d8b66ab6da8df64c51a1d81e81a58aed45794ac3
mackliu <mackliu@wda.gov.tw>
2023-01-12 09:04:44 +0800

    js time date

---
+++ s20230111/img/test.sql
@@ -0,0 +1,25 @@
+
+UPDATE `table` SET
+  `` = '',
+WHERE `` = '';
+
+
+DELETE FROM `table` WHERE `` = '';
+
+
+INSERT INTO ``(``) VALUES
+(),
+;
+
+
+UPDATE `table` SET
+  `` = '',
+WHERE `` = '';
+
+
+SELECT
+    ``.*
+  FROM `` AS ``
+
+
+DELETE FROM `table` WHERE `` = '';
