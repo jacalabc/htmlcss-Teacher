4c49bd152f7d138439652fcc9069925dc410cb96
mackliu <mackliu@wda.gov.tw>
2023-01-18 15:57:53 +0800

    curl php

--- s20230118/curl/getData.php
+++ s20230118/curl/getData.php
@@ -26,9 +26,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 // var_dump($result);
 // echo $result;
-dd($result);
+// dd($result);
 $json_decode = json_decode($result);
-dd($json_decode);
+// dd($json_decode);
 
 //關閉連線
 curl_close($ch);
